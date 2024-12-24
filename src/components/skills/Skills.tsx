01c3842f3aa10e2e9a099dba684255f68d6cb392
mayank <mayankshrma904@gmail.com>
2024-12-25 02:26:52 +0530

    Initial commit of Next.js project

--- src/components/skills/Skills.tsx
+++ src/components/skills/Skills.tsx
@@ -10,7 +10,7 @@ const Skills = () => {
             <h1 className=" text-4xl flex gap-2 items-center tracking-wide lg:text-7xl font-bold  md:border-b-2 ">
                 <Pi className=" opacity-70 lg:w-12 lg:h-12" /> Skills <span className=" w-14 h-[2px] bg-green-500"></span>
             </h1>
-            <p className=" mt-2 lg:mt-7 text-sm md:text-lg opacity-70 font-medium uppercase tracking-widest ">Crafting full stack web apps using M.E.R.N stack</p>
+            <p className=" mt-2 lg:mt-7 text-sm md:text-lg opacity-70 font-medium uppercase tracking-widest ">Crafting full stack web apps using M.E.R.N stack/NEXT JS</p>
             <div className=" md:hidden mt-4 flex gap-2 flex-wrap">
                 {skills.map((skill, index) => (
                     <span key={index} className={`text-sm font-medium tracking-wide dark:text-white text-black/70 border-2 border-black/30 dark:bg-neutral-700 dark:border-white/10 dark:shadow-lg p-2 rounded-lg mt-2 mr-2 ${index % 2 === 1 ? " rotate-3" : " -rotate-3"}`}>
